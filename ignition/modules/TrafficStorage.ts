5c2c10b4847d8434092a89705b2f46015e844525
majodkw <majodkw@gmail.com>
2025-04-26 19:16:52 -0300

    chore: Reorganize project to add python fastapi integration

--- ignition/modules/TrafficStorage.ts
+++
@@ -1,6 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("TrafficStorage", (m) => {
-  const trafficStorage = m.contract("TrafficStorage");
-  return { trafficStorage };
-});
